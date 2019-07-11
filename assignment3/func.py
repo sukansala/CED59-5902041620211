93da9154b13fa399424c22020065f17675f1c873
Sukan Sarlaring <s5902041620211@email.kmutnb.ac.th>
2019-07-11 14:39:24 +0700

    add file assignment3

---
+++ assignment3/func.py
@@ -0,0 +1,6 @@
+def return_42():
+	return 42;
+
+def my_method(x, y):
+	return x * b;
+	
---
+++ assignment3/main.py
@@ -0,0 +1,5 @@
+from func import *
+
+print("Result of return_42() is ",return_42());
+
+print("Result of 9 x 9  is ",my_method(9, 9));
