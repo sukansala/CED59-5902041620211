1c584608f64ce48e87e08526f632b790c74cfe77
sukansala <s5902041620211@email.kmutnb.ac.th>
2019-08-08 16:16:48 +0700

    homework assignment7

--- assignment7/main.py
+++ assignment7/main.py
@@ -1,7 +1,13 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+
+
+class MyForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
 
 
 @app.route("/")
@@ -9,9 +15,17 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/', methods=["post"])
-def register():
-    return 'xyz'
+#@app.route('/', methods=["post"])
+#def register():
+#    return 'xyz'
+
+
+@app.route('/', methods=('GET', 'POST'))
+def submit():
+    form = MyForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('index.html', form=form)
 
 
 if __name__ == '__main__':
